1d5761e7e60e505d25a1ef5413e39c595485f428
Phudge <70291572+Duol86@users.noreply.github.com>
2022-09-06 14:46:33 -0700

    Add files via upload

--- discord/main.py
+++ discord/main.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python
-
-#version 2.0 of KDEF, now using the pycord API wrapper
+#version 2.0 of KDEF, now using the pycord API wrapper and imroved DB
 
 #importing func.py, which contains database functions
 import func
 #<import discord> is importing package py-cord, which uses the same namespace as discord.py
-import discord, sqlite3, random
+import discord, sqlite3, random, hjson #json module can be imported instead, using file 'grammar.json'
 from discord.ext import commands
 
 c = sqlite3.connect("kygish.db")
-cu = c.cursor()
+grammar = open('grammar.hjson')
+g = hjson.load(grammar)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -48,9 +47,15 @@ async def define(ctx, word):
     #formatting
     word = word.capitalize()
     deff = func.keydef(word)
+    #deff = [word, word's language, definition's language]
 
     try:
-        await ctx.respond(f'{deff[2].capitalize()} definition: {deff[0]}')
+        await ctx.respond(embed=discord.Embed(
+            title = f'KDEF: {word}',
+            description = f'{deff[2].capitalize()} definition: {deff[0]}',
+            #description = 'english/kygish definition: word'
+            color = discord.Color.purple()
+        ))
     except:
         await ctx.respond(f'Word `{word}` not found, try checking spelling or adding the word yourself using `/add <word>`')
 
@@ -66,4 +71,16 @@ async def delete(ctx, word):
     else:
         await ctx.respond(f'Word `{word}` not found, try checking spelling or adding the word yourself using `/add <word>`')
 
+@client.slash_command(guild_ids=guilds, description='Grammar guide for Kygish')
+async def grammar(ctx, page):
+    try:
+        await ctx.respond(embed=discord.Embed(
+            title='Grammar',
+            description=g[page],
+            colour=discord.Color.purple()
+        ))
+    except:
+        await ctx.respond(f'Error: Page `{page}` is not valid')
+
+#note to self: remember to hide token when uploading to github
 client.run('<token>')
