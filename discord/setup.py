c1c48f6fc293b44f216157674aad6752d9e58774
Phudge <70291572+Duol86@users.noreply.github.com>
2022-09-06 15:06:53 -0700

    Update main branch to V2.A.0 release

--- discord/setup.py
+++ discord/setup.py
@@ -1,8 +1,27 @@
 import sqlite3
+from os import system
 
-con = sqlite3.connect('kygish.db')
+try:
+    import discord
+except:
+    print('installing dependency discord\n\n\n')
+    system('python -m pip install discord')
+    print('\n\n\ninstalled dependency discord')
 
-con.execute('''CREATE TABLE kdef
-("english" varchar,
-"kygish" varchar)''')
-con.commit()
+try:
+    import hjson
+except:
+    print('installing dependency hjson\n\n\n')
+    system('python -m pip install hjson')
+    print('\n\n\ninstalled dependency hjson')
+
+try:
+    con = sqlite3.connect('kygish.db')
+    con.execute('SELECT * FROM kdef')
+except:
+    con = sqlite3.connect('kygish.db')
+
+    con.execute('''CREATE TABLE kdef
+    ("english" varchar,
+    "kygish" varchar)''')
+    con.commit()
